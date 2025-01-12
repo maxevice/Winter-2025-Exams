775255b102b39f993289fa21b62b99f3ebe14d1f
Максим Повнич <maxympovnych@gmail.com>
2025-01-12 14:12:36 +0200

    simplify by using array method

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,12 +3,8 @@
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (day) => {
-  for (let i = 0; i < days.length; i++) {
-    if (day.startsWith(days[i].toLowerCase())) {
-      return i + 1;
-    }
-  }
-  return -1;
+  const index = days.findIndex(value => day.startsWith(value.toLowerCase()));
+  return index + 1 || -1;
 };
 
 module.exports = parseDay;
