8973e6a6475c0f0a196916fa57e71cc964ccf591
Максим Повнич <maxympovnych@gmail.com>
2025-01-12 12:33:52 +0200

    remove useless else blocks

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,17 +3,14 @@
 const getValueBetween = (str, prefix, suffix) => {
   const prefixIndex = str.indexOf(prefix);
   if (prefixIndex === -1) return '';
-  else {
-    const startPoint = prefixIndex + prefix.length;
-    str = str.substring(startPoint);
-    if (suffix) {
-      const suffixIndex = str.indexOf(suffix);
-      if (suffixIndex === -1) {
-        return '';
-      } else {
-        str = str.substring(0, suffixIndex);
-      }
+  const startPoint = prefixIndex + prefix.length;
+  str = str.substring(startPoint);
+  if (suffix) {
+    const suffixIndex = str.indexOf(suffix);
+    if (suffixIndex === -1) {
+      return '';
     }
+    str = str.substring(0, suffixIndex);
   }
   return str;
 };
