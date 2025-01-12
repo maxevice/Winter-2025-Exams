d2701ba4f8acb3d976c75900f22575231ba28c15
Максим Повнич <maxympovnych@gmail.com>
2025-01-12 13:38:06 +0200

    add 'use strict', improve naming, add const, removed useless declaration

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,14 @@
-// Refactor following solution
-// Get day number
+'use strict'
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+const parseDay = (day) => {
+  for (let i = 0; i < days.length; i++) {
+    if (day.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
