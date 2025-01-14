fc62b4ae0b3fccdc4e36ba7e755c08022ff3bb44
Максим Повнич <maxympovnych@gmail.com>
2025-01-14 12:09:32 +0200

    no need for flag, we need only one condition

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -4,14 +4,10 @@ const compare = (dict1, dict2) => {
   const dict1Keys = Object.keys(dict1);
   const dict2Keys = Object.keys(dict2);
   if (dict1Keys.join('-') !== dict2Keys.join('-')) return false;
-  let flag = true;
   for (const key of dict1Keys) {
-    if (dict1[key] === dict2[key]) flag = flag && true;
-    else {
-      flag = flag && false;
-    }
+    if (dict1[key] !== dict2[key]) return false;
   }
-  return flag;
+  return true;
 };
 
 module.exports = compare;
