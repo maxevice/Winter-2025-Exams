d9f346e58037f85272c40d546b1ed87e2d23439e
Максим Повнич <maxympovnych@gmail.com>
2025-01-14 12:19:18 +0200

    use every instead of loop

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -4,10 +4,8 @@ const compare = (dict1, dict2) => {
   const dict1Keys = Object.keys(dict1);
   const dict2Keys = Object.keys(dict2);
   if (dict1Keys.join('-') !== dict2Keys.join('-')) return false;
-  for (const key of dict1Keys) {
-    if (dict1[key] !== dict2[key]) return false;
-  }
-  return true;
-};
+  return dict1Keys.every(key => dict1[key] === dict2[key]);
+}
+
 
 module.exports = compare;
